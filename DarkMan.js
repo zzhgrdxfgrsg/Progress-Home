ab70b735bbef93a1c68f84d5e57f7aed87b04ead
zzhgrdxfgrsg <77732315+zzhgrdxfgrsg@users.noreply.github.com>
2021-02-27 09:12:56 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -21,7 +21,7 @@ var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
 const prefix = "-";
-client.login("");
+client.login("ODExNjM1NDAzMjYxODcwMDkx.YC1EXw.F0s-q2D_0wkiiWkK-bEjiam3zhA");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
